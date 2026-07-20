eb97d513583dc27b60921a745299fea3028df45c
aditya-3645 <dhumaladitya2005@gmail.com>
2026-07-20 17:38:53 +0530

    2nd commit

--- Assignment_5/app.py
+++ Assignment_5/app.py
@@ -36,15 +36,60 @@ st.write("Selected Genre:", genre)
 st.write("Selected Art Style:", art_style)
 
 prompt = f"""
-Start a {genre} adventure.
+You are an AI Visual Novel engine.
 
-Keep it around 80 words.
+Genre: {genre}
 
-The story should feel immersive.
+Art Style: {art_style}
+
+Return ONLY valid JSON.
+
+The JSON format must be:
+
+{{
+    "story_text":"Story here",
+
+    "image_prompt":"Detailed image prompt",
+
+    "options":[
+        "Choice 1",
+        "Choice 2",
+        "Choice 3"
+    ]
+}}
+
+Rules:
+
+1. Do NOT use markdown.
+
+2. Do NOT use ```.
+
+3. Return ONLY JSON.
+
+4. Story should be around 80 words.
+
+5. Image prompt should be highly detailed.
+
+6. Generate exactly 3 choices.
 """
 
 if st.button("Start Adventure"):
-
     response = st.session_state.chat.send_message(prompt)
+    data = json.loads(response.text)
+    story = data["story_text"]
+    image_prompt = data["image_prompt"]
+    options = data["options"]
 
-    st.write(response.text)
+
+
+#-----------------------------------------------------------
+st.subheader("📖 Story")
+st.write(story)
+
+st.subheader("🎨 Image Prompt")
+st.write(image_prompt)
+
+st.subheader("🎮 Choose Your Action")
+for option in options:
+    if st.button(option):
+        st.write("You selected:", option)
